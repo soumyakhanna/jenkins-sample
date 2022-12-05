f38075fdf254d50decb6d3b4f5cf1c8a2db9c630
Soumya Khanna <soumya.khanna99@gmail.com>
2022-12-05 09:49:03 -0600

    Create Sample.java

---
+++ src/Sample.java
@@ -0,0 +1,11 @@
+public class Sample {
+
+    public static void main(String args[])
+    {
+        for(int i = 0; i < 5; i++)
+        {
+            //Commit for webhooks 2
+            System.out.println("Hello World");
+        }
+    }
+}
